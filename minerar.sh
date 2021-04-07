f9db220dc6e81c40503c48387f678593952e1349
Lroberto-18 <leandroroberto582@gmail.com>
2021-04-07 16:34:52 -0300

    first

---
+++ minerar.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+echo "Digite o nome da linguagem desejada"
+read linguagem
+
+numero_linhas=$(grep -c ".*" Repositorios/$linguagem.txt)
+urls=Repositorios/$linguagem.txt
+echo "Números de repositórios: $numero_linhas ."
+cat Repositorios/$linguagem.txt
+for minerar in `cat $urls`
+    do
+        echo "minerando no link: $minerar"
+        caminho_repositorio=$(echo $minerar | sed 's/https:\/\/github.com\///g' -s)
+        repositorio=$(echo $caminho_repositorio | sed 's/\//-/g' | sed 's/.*-//g')
+		arquivo=$(echo "$linguagem\_$repositorio" | sed 's/\\//g')
+		curl -H "Accept: application/vnd.github.v3+json"  https://api.github.com/repos/"$caminho_repositorio"/pulls?state=closed | grep login | head -200| sed 's/"login": "//g' | sed 's/[", ]//g' | sort | uniq -c | sort -n | sort -nr | awk '{ print $2 "," $1}' >$arquivo.csv
+    done
